f3b4f3f92b551b487ab35f9c096df8ecd9ae5432
Mehboob-Khan <mehboobkh8n@gmail.com>
2023-03-16 18:27:51 +0000

    update

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,38 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:provider/provider.dart';
-import '../Models/Movie/movie_model.dart';
-import '../http/http_request.dart';
 
+import '../constant/style.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    PageController _pageController = PageController();
+    void _onPageChanged(int index) {
+      setState(() {
+        _currentIndex = index;
+      });
+      _pageController.animateToPage(index, duration: const Duration(milliseconds: 200), curve: Curves.easeIn);
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
+      appBar: AppBar(title: _buildTitle(_currentIndex)),
+      body: PageView(
+        controller: _pageController, 
+        children: const <Widget>[
+          Center(child: Text('Home')),
+          Center(child: Text('Favorite')),
+        ],
       ),
-      body: FutureBuilder<MovieModel>(
-        future: HttpRequest.getMovies("popular"),
-        builder: (BuildContext context, AsyncSnapshot<MovieModel> snapshot) {
-          if (snapshot.hasData) {
-            return _buildContentWidget(snapshot.data);
-          }
-          return Container();
-        },
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Style.primaryColor,
+        selectedItemColor: Style.secondColor,
+        unselectedItemColor: Style.textColor,
+        onTap: _onPageChanged,
+        currentIndex: _currentIndex,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorite'),
+        ],
       ),
     );
   }
-  Widget _buildContentWidget(MovieModel? movies) {
-    return Container(
-      child: Center(
-        child: Text("${movies!.movies![0].title}"),
-      ),
-    );
+
+  _buildTitle(int index)
+  {
+    switch (index) {
+      case 0:
+        return const Text('Movie Shows');
+      case 1:
+        return const Text('Favorite');
+      default:
+        return null;
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'package:tmdb_application/constant/style.dart';
 import 'Screens/home_screen.dart';
+
 
 Future<void> main() async {
   await dotenv.load();
@@ -14,10 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Movie App',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Style.primaryColor,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Style.primaryColor,
+            elevation: 0,
+          ),
         ),
-        home: HomeScreen());
+        home: const HomeScreen());
   }
 }
