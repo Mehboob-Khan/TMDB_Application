90f573a1053094424970b3e9395d637b854ca1a4
Mehboob-Khan <mehboobkh8n@gmail.com>
2023-03-16 20:16:38 +0000

    Updated

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,56 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_application/Screens/movie_screen.dart';
 
 import '../constant/style.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _currentIndex = 0;
-
+  late int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    PageController _pageController = PageController();
-    void _onPageChanged(int index) {
-      setState(() {
-        _currentIndex = index;
-      });
-      _pageController.animateToPage(index, duration: const Duration(milliseconds: 200), curve: Curves.easeIn);
+    // creatimg a page controller
+    PageController _controller = PageController();
+    void onTapIcon(int index) {
+      _controller.animateToPage(index,
+          duration: const Duration(milliseconds: 200), curve: Curves.easeIn);
     }
 
     return Scaffold(
-      appBar: AppBar(title: _buildTitle(_currentIndex)),
+      appBar: AppBar(
+        title: _buildTitle(_currentIndex),
+      ),
       body: PageView(
-        controller: _pageController, 
+        controller: _controller,
         children: const <Widget>[
-          HomeScreen(),
-          Center(child: Text('Favorite')),
+          MovieScreen(),
+          
+          Center(
+            child: Text('Watch list', style: TextStyle(color: Style.textColor)),
+          ),
         ],
+        onPageChanged: (value) => setState(() {
+          _currentIndex = value;
+        }),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Style.primaryColor,
         selectedItemColor: Style.secondColor,
         unselectedItemColor: Style.textColor,
-        onTap: _onPageChanged,
+        onTap: onTapIcon,
         currentIndex: _currentIndex,
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorite'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            label: 'Movie',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Watchlist',
+          ),
         ],
       ),
     );
   }
 
-  _buildTitle(int index)
-  {
-    switch (index) {
+  _buildTitle(int _index) {
+    switch (_index) {
       case 0:
         return const Text('Movie Shows');
       case 1:
-        return const Text('Favorite');
+        return const Text('Watchlist');
       default:
         return null;
     }
