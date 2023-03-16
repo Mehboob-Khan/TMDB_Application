039b400e8b24ccd443a69d25e99ddf3eab03e6ee
Mehboob-Khan <mehboobkh8n@gmail.com>
2023-03-16 16:15:54 +0000

    Refactor MovieDetailsModel and MovieDetails with improved logic and methods

---
+++ lib/Models/Movie/movie_details_model.dart
@@ -0,0 +1,60 @@
+class MovieDetailsModel {
+  final MovieDetails? details;
+  final String? error;
+
+  MovieDetailsModel({required this.details, required this.error});
+
+  factory MovieDetailsModel.fromJson(Map<String, dynamic> json) {
+    return MovieDetailsModel(
+      details: MovieDetails.fromJson(json),
+      error: "",
+    );
+  }
+
+  factory MovieDetailsModel.withError(String error) {
+    return MovieDetailsModel(
+      details: null,
+      error: error,
+    );
+  }
+}
+
+class MovieDetails {
+  final int? id;
+  final List<Genre>? genres;
+  final String? releaseDate;
+  final String? overview;
+  final String? backDrop;
+  final String? poster;
+  final double? rating;
+  final String? title;
+  final int? runtime;
+
+  MovieDetails({
+    required this.id,
+    required this.genres,
+    required this.releaseDate,
+    required this.overview,
+    required this.backDrop,
+    required this.poster,
+    required this.rating,
+    required this.title,
+    required this.runtime,
+  });
+
+  factory MovieDetails.fromJson(Map<String, dynamic> json) {
+    return MovieDetails(
+      id: json['id'],
+      genres: (json['genres'] as List)
+          .map((genre) => Genre.fromJson(genre))
+          .toList(),
+      rating: json['vote_average'].toDouble(),
+      title: json['title'],
+      backDrop: json['backdrop_path'],
+      poster: json['poster_path'],
+      overview: json['overview'],
+      releaseDate: json['release_date'],
+      runtime: json['runtime'],
+    );
+  }
+}
