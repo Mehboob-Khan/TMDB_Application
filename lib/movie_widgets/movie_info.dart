a7f24a2a06b5ebbfc9144a82946f5c3763b1b39a
Mehboob-Khan <mehboobkh8n@gmail.com>
2023-03-17 01:04:11 +0000

    Rating and Release Date

--- lib/movie_widgets/movie_info.dart
+++ lib/movie_widgets/movie_info.dart
@@ -1,9 +1,6 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:tmdb_application/Models/Movie/movie_details_model.dart';
-
 import '../constant/style.dart';
 import '../http/http_request.dart';
 
@@ -77,87 +74,114 @@ class _MovieInfoState extends State<MovieInfo> {
       _buildRating(details),
     ]);
   }
-Widget _buildRating(MovieDetails details) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 20),
-    child: Row(
-      children: <Widget>[
-        const SizedBox(
-          width: 10,
-        ),
-        Expanded(
-          child: SizedBox(
-            height: 120,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 30),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        details.rating!.toString(),
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
+
+  Widget _buildRating(MovieDetails details) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: <Widget>[
+          const SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: SizedBox(
+              height: 120,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10), // Changed padding here
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          details.rating!.toString(),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        width: 15,
-                      ),
-                      RatingBar.builder(
-                        itemSize: 20,
-                        initialRating: details.rating! / 2,
-                        minRating: 1,
-                        direction: Axis.horizontal,
-                        allowHalfRating: true,
-                        itemCount: 5,
-                        itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
-                        itemBuilder: (context, _) {
-                          return const Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                          );
-                        },
-                        onRatingUpdate: (rating) {},
-                      ),
-                    ],
+                        const SizedBox(
+                          width: 15,
+                        ),
+                        RatingBar.builder(
+                          itemSize: 20,
+                          initialRating: details.rating! / 2,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding:
+                              const EdgeInsets.symmetric(horizontal: 2.0),
+                          itemBuilder: (context, _) {
+                            return const Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            );
+                          },
+                          onRatingUpdate: (rating) {},
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const Text(
-                        "DURATION",
-                        style: TextStyle(
-                          color: Style.textColor,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 12,
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10), // Changed padding here
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const Text(
+                          "DURATION",
+                          style: TextStyle(
+                            color: Style.textColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12,
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        '${details.runtime!} mins',
-                        style: const TextStyle(
-                          color: Style.secondColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
+                        const SizedBox(
+                          height: 10,
                         ),
-                      ),
-                    ],
+                        Text(
+                          '${details.runtime!} mins',
+                          style: const TextStyle(
+                            color: Style.secondColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text(
+                          "RELEASE DATE",
+                          style: TextStyle(
+                            color: Style.textColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          '${details.releaseDate!}',
+                          style: const TextStyle(
+                            color: Style.secondColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
+  }
 }
