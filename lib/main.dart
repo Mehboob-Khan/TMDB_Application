a66eb41bc53a0790c35e9e9ef52b424792ce27b2
Mehboob-Khan <mehboobkh8n@gmail.com>
2023-03-18 02:13:25 +0000

    latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:tmdb_application/constant/style.dart';
 import 'Models/Movie/hive_movie_model.dart';
 import 'Screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load();
   await Hive.initFlutter();
   Hive.registerAdapter(HiveMovieModelAdapter());
